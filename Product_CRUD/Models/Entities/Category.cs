37f2aea742cf3c846f5efeec00f6a8d684e811ab
asdros <szymonbraun@tlen.pl>
2022-05-16 20:50:39 +0200

    create models

---
+++ Product_CRUD/Models/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product_CRUD.Models.Entities
+{
+    public class Category
+    {
+        [Column("CategoryId")]
+        public short Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa kategorii jest wymagana")]
+        [MaxLength(20, ErrorMessage = "Maksymalna długość to 20 znaków.")]
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Product_CRUD/Models/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product_CRUD.Models.Entities
+{
+    public class Product
+    {
+        [Column("ProductId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa produktu jest wymagana.")]
+        [MaxLength(50, ErrorMessage = "Maksymalna długość to 50 znaków.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Kategoria jest wymagana.")]
+        [Range(0, short.MaxValue, ErrorMessage = "Identyfikator kategorii musi być liczbą, mniejszą niż 32767.")]
+        [ForeignKey(nameof(Category))]
+        public short CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        private decimal _price;
+
+        [Required(ErrorMessage = "Cena produktu jest wymagana.")]
+        [RegularExpression(@"^\d+(.\d{1,2})?$", ErrorMessage = "Nieprawidłowa cena.")]
+        public decimal Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                _price = value * 1.23M;         //adding 23% tax during adding a new product to a database
+            }
+        }
+    }
+}
